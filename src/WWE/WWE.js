2b2f8d947d39ff8107a5f17c9785aa59f07b259e
cod3rcarl <cod3rcarl@gmail.com>
2021-01-30 11:19:22 +0000

    bug fixing

--- src/WWE/WWE.js
+++ src/WWE/WWE.js
@@ -9,7 +9,7 @@ const WWE = () => {
   const [date, setDate] = useState("");
 
   async function getWrestlerInfo() {
-    const res = await fetch(`${url}wrestlers`);
+    const res = await fetch(`${url}`);
     const data = await res.json();
     data.payload.forEach((item) => {
       const dateLost = item.datelost;
