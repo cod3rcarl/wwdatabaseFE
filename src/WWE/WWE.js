6e4dd812cdafea1ad931eed10e87cc37e27b093b
cod3rcarl <cod3rcarl@gmail.com>
2021-01-30 16:44:22 +0000

    added error messages for invalid dates

--- src/WWE/WWE.js
+++ src/WWE/WWE.js
@@ -5,44 +5,58 @@ const url =
   process.env.REACT_APP_BACKEND_URL || `https://wwdatabase.herokuapp.com`;
 const WWE = () => {
   const [wrestler, setWrestler] = useState("Superstar appears here");
-  const [isLoading, setIsLoading] = useState(true);
   const [date, setDate] = useState("");
 
   async function getWrestlerInfo() {
-    const res = await fetch(`${url}`);
+    const res = await fetch(`${url}wrestlers`);
     const data = await res.json();
-    data.payload.forEach((item) => {
+
+    data.payload.filter((item) => {
       const dateLost = item.datelost;
       const dateWon = item.datewon;
+      const firstDateWon = "04/11/1963";
 
       let D_1 = dateLost.split("/");
       let D_2 = dateWon.split("/");
       let D_3 = date.split("/");
+      let D_4 = firstDateWon.split("/");
 
       let dlost = new Date(D_1[2], parseInt(D_1[1]) - 1, D_1[0]);
       let dwon = new Date(D_2[2], parseInt(D_2[1]) - 1, D_2[0]);
-      let d3 = new Date(D_3[2], parseInt(D_3[1]) - 1, D_3[0]);
+      let inputDate = new Date(D_3[2], parseInt(D_3[1]) - 1, D_3[0]);
+      let firstDate = new Date(D_4[2], parseInt(D_4[1]) - 1, D_4[0]);
+      let today = new Date();
 
-      if (d3 < dlost && d3 > dwon) {
-        console.log("success");
+      let input = document.getElementById("dateInput");
+      input.value = "";
 
-        setWrestler(item.champion);
-        setIsLoading(false);
+      if (inputDate < firstDate || inputDate > today) {
+        setWrestler("This date is not in the database");
+        setDate(input);
       }
+      // var d2 = new Date(d1);
+      // var same = d1.getTime() === d2.getTime();
+      // var notSame = d1.getTime() !== d2.getTime();
+
+      if (inputDate < dlost && inputDate > dwon) {
+        setWrestler(item.champion);
+        setDate(input);
+      }
+      return wrestler;
     });
   }
   function handleClick() {
     const myRegex = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
-
-    if (date.match(myRegex)) {
-      console.log("success");
-      getWrestlerInfo();
-    } else {
-      alert(" Invalid input! Please input date format as DD/MM/YYYY");
-    }
     let input = document.getElementById("dateInput");
     input.value = "";
-    setDate(input);
+
+    if (!date.match(myRegex)) {
+      setWrestler("Invalid input! Please input date format as DD/MM/YYYY");
+      setDate(input);
+    } else {
+      console.log("success");
+      getWrestlerInfo();
+    }
   }
   return (
     <section>
@@ -62,18 +76,11 @@ const WWE = () => {
       </div>
       <div className="container">
         {" "}
-        {isLoading ? (
-          <p
-            placeholder=" ...Loading Superstar will appear here"
-            className="superstar"
-          >
-            ...Loading Superstar will appear here
-          </p>
-        ) : (
-          <p placeholder="Superstar appears here" className="superstar">
-            {wrestler}
-          </p>
-        )}
+        (
+        <p placeholder="Superstar appears here" className="superstar">
+          {wrestler}
+        </p>
+        )
       </div>
       <div className="container">
         {" "}
